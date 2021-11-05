e6942a156b9d393ca9ac2845bf8993d80121d2c2
JohnKotronakis <williamkotronakis@gmail.com>
2021-11-05 09:32:14 +0200

    feat: added initial persistent store logic, WIP

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootReducer from './reducer';
 import rootSaga from './saga';
+// import { loadState, saveState } from "../utils/localStorage";
 
 const sagaMiddleware = createSagaMiddleware();
 
+// const persistedState = loadState();
+
 const store = configureStore({
     reducer: rootReducer,
+    // preloadedState: persistedState,
     middleware: getDefaultMiddleware => getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware)
 });
 
 sagaMiddleware.run(rootSaga);
+
+// store.subscribe(() => {
+//     saveState(store.getState());
+// });
 
 export default store;
