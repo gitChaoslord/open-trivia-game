f59200a0ac77ddc8e136535531a6bcff0e5a3897
JohnKotronakis <williamkotronakis@gmail.com>
2021-12-13 00:51:57 +0200

    fix: display current settings in initial page

--- src/pages/InitialPage.tsx
+++ src/pages/InitialPage.tsx
@@ -12,8 +12,17 @@ const InitialPage: React.FC = () => {
     }
 
     return (
-        <div className="page-content mt-80">
-            <span> select values yada yada</span>
+        <div className="page-content mt-20">
+            <h1 className="text-4xl text-indigo-500 text-center">Current settings</h1>
+            <div className="lg:w-auto  border-b-2 text-lg border-indigo-500 flex justify-between bg-white my-6 rounded ">
+                <ul className="p-5">
+                    <li className="mb-2">Number of questions: <span className="text-indigo-500">10</span></li>
+                    <li className="mb-2">Category: <span className="text-indigo-500">Any</span></li>
+                    <li className="mb-2">Difficulty: <span className="text-indigo-500">Any</span></li>
+                    <li className="mb-2">Type: <span className="text-indigo-500">True/False</span></li>
+                    <li>Time: <span className="text-indigo-500">60 sec.</span></li>
+                </ul>
+            </div>
             <Button onClick={startGameHandler}>Start Game</Button>
         </div>
     )
