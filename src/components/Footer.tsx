ccee652dc5e64e9e71b6b56002d8c0ebd4a468a7
John Kotronakis <williamkotronakis@gmail.com>
2022-03-13 19:00:10 +0200

    chore: added version to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { version } from '../../package.json';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-indigo-500 justify-self-end p-3">
-      <span className="text-white">by <a href="https://gitchaoslord.github.io" target="_blank" rel="noreferrer" >John Kotronakis</a> </span>
+    <footer className="bg-indigo-500 justify-self-end p-1">
+      <p className="text-gray-300">by <a href="https://gitchaoslord.github.io" className="text-white" target="_blank" rel="noreferrer" >John Kotronakis</a> </p>
+      <span className="text-sm text-gray-300">Version - {version}</span>
     </footer>
   )
 }
