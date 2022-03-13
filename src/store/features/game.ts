900ef36611924f5d7a54cb875ecd2270ed7e36eb
John Kotronakis <williamkotronakis@gmail.com>
2022-03-13 19:03:03 +0200

    chore: removed unused reducer from game slice

--- src/store/features/game.ts
+++ src/store/features/game.ts
@@ -1,19 +1,12 @@
 import { createSlice, PayloadAction, Slice } from "@reduxjs/toolkit";
-import { GameSettings, Stage } from "../../models/Game";
+import { Stage } from "../../models/Game";
 
 export interface GameState {
   stage: Stage;
-  settings: GameSettings;
 }
 
 const initialState: GameState = {
   stage: 'INIT',
-  settings: {
-    questions: 10,
-    difficulty: 'any',
-    category: 10,
-    type: 'all'
-  }
 }
 
 const gameSlice: Slice = createSlice({
@@ -23,11 +16,8 @@ const gameSlice: Slice = createSlice({
     setStage: (state: GameState, action: PayloadAction<Stage>) => {
       state.stage = action.payload;
     },
-    setSettings: (state: GameState, action: PayloadAction<GameSettings>) => {
-      state.settings = action.payload;
-    },
   },
 });
 
-export const { setStage, setSettings } = gameSlice.actions;
+export const { setStage } = gameSlice.actions;
 export default gameSlice.reducer;
