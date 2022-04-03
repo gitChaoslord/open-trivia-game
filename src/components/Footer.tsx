7e34a5158dcf6aaf623c2ae16da04ee2ea647ea9
John Kotronakis <williamkotronakis@gmail.com>
2022-04-03 21:30:07 +0300

    chore: updated tailwind and react-scripts, removed unused packages and removed version import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { version } from '../../package.json';
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-indigo-500 justify-self-end p-1">
       <p className="text-gray-300">by <a href="https://gitchaoslord.github.io" className="text-white" target="_blank" rel="noreferrer" >John Kotronakis</a> </p>
-      <span className="text-sm text-gray-300">Version - {version}</span>
     </footer>
   )
 }
