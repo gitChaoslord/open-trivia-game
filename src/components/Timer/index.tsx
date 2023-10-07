f98b4f5c8e1ae19c911f026608cb23598dd4e0b4
william <williamkotronakis@gmail.com>
2023-10-07 18:08:17 +0300

    style: defined basic timer classes

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -11,16 +11,30 @@ const Timer: React.FC = () => {
   const offset = circumference * ((gameDuration - timeLeft) / gameDuration);
 
   // TODO: add color variables and dark/light theme
-
   return (
     <svg width="160" height="160" viewBox="0 0 160 160" style={{ rotate: "-90deg" }}>
-      <circle r="68" cx="80" cy="80" fill="transparent" stroke="#e0e0e0" stroke-width="22px" />
-      <circle r="68" cx="80" cy="80" fill="transparent" stroke="#60e6a8" stroke-width="12px" stroke-dasharray={circumference} stroke-dashoffset={offset} />
-
+      <circle
+        className="timer-background"
+        r="68"
+        cx="80"
+        cy="80"
+        fill="transparent"
+        stroke-width="22px"
+      />
+      <circle
+        className="timer-bar"
+        r="68"
+        cx="80"
+        cy="80"
+        fill="transparent"
+        stroke-width="12px"
+        stroke-dasharray={circumference}
+        stroke-dashoffset={offset}
+      />
       <text
+        className="timer-text"
         x="50%"
         y="50%"
-        fontSize={52}
         dominant-baseline="central"
         text-anchor="middle"
         style={{ rotate: "90deg", transformOrigin: '50% 50%' }}>
