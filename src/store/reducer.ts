d54d554fd9cacf437883623b88028d61fe888825
william <williamkotronakis@gmail.com>
2024-02-09 16:35:23 +0200

    chore: dependency update

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,6 +1,6 @@
 import type { GameState } from "@models/game";
 import { SettingsState } from "@models/settings";
-import { AnyAction, combineReducers } from "@reduxjs/toolkit";
+import { Action, combineReducers } from "@reduxjs/toolkit";
 import game from "@store/features/game";
 import persistReducer from "redux-persist/es/persistReducer";
 import storage from "redux-persist/lib/storage";
@@ -13,13 +13,13 @@ const persistConfig = {
 }
 
 export default combineReducers({
-  game: persistReducer<GameState, AnyAction>({
+  game: persistReducer<GameState, Action>({
     ...persistConfig,
     key: persistConfig.key + 'game',
     blacklist: ['loading', 'categoriesInitialized', 'categoriesLoading', 'categories']
   },
     game),
-  settings: persistReducer<SettingsState, AnyAction>({
+  settings: persistReducer<SettingsState, Action>({
     ...persistConfig,
     key: persistConfig.key + 'settings',
     blacklist: ['settingsModalOpen']
